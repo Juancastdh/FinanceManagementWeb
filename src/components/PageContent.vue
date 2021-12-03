ea5d100ca15a5a0783ca18c9b01d22492b661df3
Juan Camilo Sánchez Taveras <juancastdh@gmail.com>
2021-12-02 21:09:16 -0400

    Added keep alive to page content components to reduce calls to the API

--- src/components/PageContent.vue
+++ src/components/PageContent.vue
@@ -2,7 +2,11 @@
   <section class="section">
     <div class="row">
       <div class="col-lg-12">
-        <router-view/>
+        <router-view v-slot="{ Component }">
+          <keep-alive>
+            <component :is="Component" :key="$route.fullPath"></component>
+          </keep-alive>
+        </router-view>
       </div>
     </div>
   </section>
