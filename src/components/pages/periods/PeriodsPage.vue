120227958f7e4608bbc4fe24f0df7647536bdcbf
Juan Camilo Sánchez Taveras <juancastdh@gmail.com>
2024-05-31 16:57:41 -0400

    Migrated periodsPage to Typescript and Composition API

---
+++ src/components/pages/periods/PeriodsPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <PeriodsTable ref="periodsTable" />
+        <AddPeriod v-on:period-added="refreshPeriodsTable" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import PeriodsTable from './componentes/PeriodsTable.vue';
+import AddPeriod from './componentes/AddPeriod.vue';
+
+const periodsTable = ref();
+
+async function refreshPeriodsTable() {
+    await periodsTable.value.refresh();
+}
+
+</script>
