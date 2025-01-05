ab1c658fdc63322dacd6fff831ea71c942484e4f
Juan Camilo Sanchez Taveras <juancastdh@gmail.com>
2025-01-04 23:54:35 -0400

    Added accounts page

---
+++ src/components/pages/accounts/AccountsPage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <AccountsTable ref="accountsTable" v-on:account-edited="setAccountToEdit"/>
+      <AddAccount ref="addAccount" v-on:account-added="refreshAccountsTable" />
+    </div>
+  </template>
+  
+  <script>
+  import AccountsTable from "./components/AccountsTable.vue";
+  import AddAccount from "./components/AddAccount.vue";
+  
+  export default {
+    name: "AccountsPage",
+    components: {
+      AccountsTable,
+      AddAccount
+    },
+    methods: {
+      refreshAccountsTable: function () {
+        this.$refs.accountsTable.refresh();
+      },
+      setAccountToEdit: function(account){
+        this.$refs.addAccount.setAccount(account);
+      }
+    },
+  };
+  </script>
