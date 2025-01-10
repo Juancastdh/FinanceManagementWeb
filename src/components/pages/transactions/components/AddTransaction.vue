54bb7c10fad03ed8f76375f59953cd9c6ccc5ffc
Juan Camilo Sanchez Taveras <juancastdh@gmail.com>
2025-01-10 15:35:37 -0400

    Added account selector to add transaction form

--- src/components/pages/transactions/components/AddTransaction.vue
+++ src/components/pages/transactions/components/AddTransaction.vue
@@ -6,17 +6,10 @@
         <form class="row g-3 needs-validation" novalidate>
           <div class="col-md-6">
             <div class="form-floating">
-              <input
-                type="date"
-                class="form-control"
-                id="transactionDateField"
-                v-model="transaction.date"
-                v-bind:class="{
-                  'is-invalid': !validDate() && attemptedToSubmit,
-                  'is-valid': validDate() && attemptedToSubmit,
-                }"
-                required
-              />
+              <input type="date" class="form-control" id="transactionDateField" v-model="transaction.date" v-bind:class="{
+                'is-invalid': !validDate() && attemptedToSubmit,
+                'is-valid': validDate() && attemptedToSubmit,
+              }" required />
               <label for="floatingName">Date</label>
               <div class="invalid-feedback">
                 Please specify a transaction date that is within an existing
@@ -26,39 +19,24 @@
           </div>
           <div class="col-md-6">
             <div class="form-floating">
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Description"
-                id="transactionDescriptionField"
-                v-model="transaction.description"
-                v-bind:class="{
+              <input type="text" class="form-control" placeholder="Description" id="transactionDescriptionField"
+                v-model="transaction.description" v-bind:class="{
                   'form-control': true,
                   'is-invalid': !validDescription() && attemptedToSubmit,
                   'is-valid': validDescription() && attemptedToSubmit,
-                }"
-                required
-              />
+                }" required />
               <label for="floatingName">Description</label>
               <div class="invalid-feedback">Please specify a description.</div>
             </div>
           </div>
           <div class="col-md-6">
             <div class="form-floating">
-              <input
-                type="number"
-                class="form-control"
-                placeholder="Value"
-                id="transactionValueField"
-                step=".01"
-                v-model="transaction.value"
-                v-bind:class="{
+              <input type="number" class="form-control" placeholder="Value" id="transactionValueField" step=".01"
+                v-model="transaction.value" v-bind:class="{
                   'form-control': true,
                   'is-invalid': !validValue() && attemptedToSubmit,
                   'is-valid': validValue() && attemptedToSubmit,
-                }"
-                required
-              />
+                }" required />
               <label for="floatingName">Value</label>
               <div class="invalid-feedback">Please specify a value.</div>
             </div>
@@ -67,22 +45,12 @@
 
           <div class="col-md-6">
             <div class="form-floating mb-3">
-              <select
-                class="form-select"
-                id="categoryCombobox"
-                aria-label="Category"
-                v-model="transaction.categoryId"
+              <select class="form-select" id="categoryCombobox" aria-label="Category" v-model="transaction.categoryId"
                 v-bind:class="{
                   'is-invalid': !validCategory() && attemptedToSubmit,
                   'is-valid': validCategory() && attemptedToSubmit
-                }"
-                :disabled = "isSplitIntoCategoriesSelected()"
-              >
-                <option
-                  v-for="category in categories"
-                  :key="category.id"
-                  :value="category.id"
-                >
+                }" :disabled="isSplitIntoCategoriesSelected()">
+                <option v-for="category in categories" :key="category.id" :value="category.id">
                   {{ category.name }}
                 </option>
               </select>
@@ -97,37 +65,21 @@
             <legend class="col-form-label col-sm-2 pt-0">Split into all categories?</legend>
             <div class="col-sm-6">
               <div class="form-check">
-                <input
-                  class="form-check-input"
-                  type="radio"
-                  name="splitRadios"
-                  value="true"
-                  id="splitIntoAllCategories"
-                  v-model="splitIntoCategories"
-                  v-bind:class="{
+                <input class="form-check-input" type="radio" name="splitRadios" value="true" id="splitIntoAllCategories"
+                  v-model="splitIntoCategories" v-bind:class="{
                     'is-invalid': !validSplitOption() && attemptedToSubmit,
                     'is-valid': validSplitOption() && attemptedToSubmit
-                  }"
-                  required
-                />
+                  }" required />
                 <label class="form-check-label" for="splitIntoAllCategories">
                   Yes
                 </label>
               </div>
               <div class="form-check">
-                <input
-                  class="form-check-input"
-                  type="radio"
-                  name="splitRadios"
-                  value="false"
-                  id="doNotSplitIntoAllCategories"
-                  v-model="splitIntoCategories"
-                  v-bind:class="{
+                <input class="form-check-input" type="radio" name="splitRadios" value="false"
+                  id="doNotSplitIntoAllCategories" v-model="splitIntoCategories" v-bind:class="{
                     'is-invalid': !validSplitOption() && attemptedToSubmit,
                     'is-valid': validSplitOption() && attemptedToSubmit,
-                  }"
-                  required
-                />
+                  }" required />
                 <label class="form-check-label" for="doNotSplitIntoAllCategories">
                   No
                 </label>
@@ -140,37 +92,21 @@
             <legend class="col-form-label col-sm-2 pt-0">Type</legend>
             <div class="col-sm-6">
               <div class="form-check">
-                <input
-                  class="form-check-input"
-                  type="radio"
-                  name="typeRadios"
-                  value="false"
-                  id="transactionIncomeType"
-                  v-model="transaction.isExpense"
-                  v-bind:class="{
+                <input class="form-check-input" type="radio" name="typeRadios" value="false" id="transactionIncomeType"
+                  v-model="transaction.isExpense" v-bind:class="{
                     'is-invalid': !validType() && attemptedToSubmit,
                     'is-valid': validType() && attemptedToSubmit,
-                  }"
-                  required
-                />
+                  }" required />
                 <label class="form-check-label" for="transactionIncomeType">
                   Income
                 </label>
               </div>
               <div class="form-check">
-                <input
-                  class="form-check-input"
-                  type="radio"
-                  name="typeRadios"
-                  value="true"
-                  id="transactionExpenseType"
-                  v-model="transaction.isExpense"
-                  v-bind:class="{
+                <input class="form-check-input" type="radio" name="typeRadios" value="true" id="transactionExpenseType"
+                  v-model="transaction.isExpense" v-bind:class="{
                     'is-invalid': !validType() && attemptedToSubmit,
                     'is-valid': validType() && attemptedToSubmit,
-                  }"
-                  required
-                />
+                  }" required />
                 <label class="form-check-label" for="transactionExpenseType">
                   Expense
                 </label>
@@ -179,22 +115,24 @@
             </div>
           </div>
 
+          <div class="col-md-6">
+            <div class="form-floating mb-3">
+              <select class="form-select" id="accountCombobox" aria-label="Account"
+                v-model="transaction.accountIdentifier">
+                <option v-for="account in accounts" :key="account.identifier" :value="account.identifier">
+                  {{ account.description }}
+                </option>
+              </select>
+              <label for="accountCombobox">Account</label>
+            </div>
+          </div>
+
           <div class="text-center">
-            <button
-              type="button"
-              class="btn btn-primary"
-              id="addTransactionButton"
-              v-on:click="addTransaction"
-            >
+            <button type="button" class="btn btn-primary" id="addTransactionButton" v-on:click="addTransaction">
               Submit
             </button>
             <div class="divider" />
-            <button
-              type="reset"
-              class="btn btn-secondary"
-              id="resetButton"
-              v-on:click="clearForm"
-            >
+            <button type="reset" class="btn btn-secondary" id="resetButton" v-on:click="clearForm">
               Reset
             </button>
           </div>
@@ -210,7 +148,7 @@ import { periodsService } from "../../../../services/periodsService.js";
 
 export default {
   name: "AddTransaction",
-  props: ["categories"],
+  props: ["categories", "accounts"],
   data: function () {
     return {
       transaction: {
@@ -220,6 +158,7 @@ export default {
         categoryId: null,
         isExpense: null,
         periodId: null,
+        accountIdentifier: null
       },
       attemptedToSubmit: false,
       splitIntoCategories: false,
@@ -233,8 +172,8 @@ export default {
       var transactionsToBeAdded = self.getTransactionsToBeAdded();
       var transactionPromises = [];
 
-      if(transactionsToBeAdded.length > 0){
-        transactionsToBeAdded.forEach(function(transaction){
+      if (transactionsToBeAdded.length > 0) {
+        transactionsToBeAdded.forEach(function (transaction) {
           transactionPromises.push(transactionsService.addTransaction(transaction));
         });
 
@@ -242,28 +181,28 @@ export default {
           self.$emit("transaction-added");
           self.clearForm();
         });
-      }     
+      }
 
 
     },
-    getTransactionsToBeAdded: function (){
+    getTransactionsToBeAdded: function () {
       var self = this;
       var transactionsToBeAdded = [];
       if (self.isFormValid() == true) {
         var selectedPeriod = this.getSelectedPeriod();
         self.transaction.periodId = selectedPeriod.id;
         self.transaction.isExpense = (self.transaction.isExpense === 'true');
-      
-        if (self.splitIntoCategories == false){
+
+        if (self.splitIntoCategories == false) {
           transactionsToBeAdded.push(self.transaction);
         }
         else {
           var categories = self.$props.categories;
-          categories.forEach(function(category){
+          categories.forEach(function (category) {
             var categoryTransaction = {};
             Object.assign(categoryTransaction, self.transaction);
             categoryTransaction.categoryId = category.id;
-            categoryTransaction.value = categoryTransaction.value * (category.percentage/100);
+            categoryTransaction.value = categoryTransaction.value * (category.percentage / 100);
             transactionsToBeAdded.push(categoryTransaction);
           });
         }
@@ -347,10 +286,10 @@ export default {
         (p) => date >= new Date(p.startDate) && date < new Date(p.endDate)
       );
     },
-    isSplitIntoCategoriesSelected: function(){
+    isSplitIntoCategoriesSelected: function () {
 
       var isSplitIntoCategoriesSelected = false;
-      if(this.splitIntoCategories == "true"){
+      if (this.splitIntoCategories == "true") {
         isSplitIntoCategoriesSelected = true;
       }
 
