cf74d32b1eafa5f2c4cb7423370c18406863a177
Juan Camilo Sanchez Taveras <juancastdh@gmail.com>
2026-07-20 18:09:16 -0400

    Added main investment fund categories page component

---
+++ src/components/pages/investmentFundCategories/InvestmentFundCategoriesPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <InvestmentFundsTable ref="investmentFundCategoriesTable" v-on:investment-fund-category-edited="setInvestmentFundCategoryToEdit"/>
+    <AddInvestmentFundCategory ref="addInvestmentFundCategory" v-on:investment-fund-category-added="refreshInvestmentFundCategoriesTable" v-on:investment-fund-category-updated="refreshInvestmentFundCategoriesTable" />
+  </div>
+</template>
+
+<script>
+import InvestmentFundCategoriesTable from "./components/InvestmentFundCategoriesTable.vue";
+import AddInvestmentFundCategory from "./components/AddInvestmentFundCategory.vue";
+
+export default {
+  name: "InvestmentFundCategoriesPage",
+  components: {
+    InvestmentFundCategoriesTable,
+    AddInvestmentFundCategory
+  },
+  methods: {
+    refreshInvestmentFundCategoriesTable: function () {
+      this.$refs.investmentFundCategoriesTable.refresh();
+    },
+    setInvestmentFundCategoryToEdit: function(investmentFundCategory){
+      this.$refs.addInvestmentFundCategory.setInvestmentFundCategory(investmentFundCategory);
+    }
+  },
+};
+</script>
