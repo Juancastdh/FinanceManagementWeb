139e6271b146dd799d9ff21da60b85997c23c392
Juan Camilo Sanchez Taveras <juancastdh@gmail.com>
2026-07-20 09:27:47 -0400

    Added investment funds page component

---
+++ src/components/pages/investmentFunds/InvestmentFundsPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <InvestmentFundsTable ref="investmentFundsTable" v-on:investment-fund-edited="setInvestmentFundToEdit"/>
+    <AddInvestmentFund ref="addInvestmentFund" v-on:investment-fund-added="refreshInvestmentFundsTable" v-on:investment-fund-updated="refreshInvestmentFundsTable" />
+  </div>
+</template>
+
+<script>
+import InvestmentFundsTable from "./components/InvestmentFundsTable.vue";
+import AddInvestmentFund from "./components/AddInvestmentFund.vue";
+
+export default {
+  name: "InvestmentFundsPage",
+  components: {
+    InvestmentFundsTable,
+    AddInvestmentFund
+  },
+  methods: {
+    refreshInvestmentFundsTable: function () {
+      this.$refs.investmentFundsTable.refresh();
+    },
+    setInvestmentFundToEdit: function(investmentFund){
+      this.$refs.addInvestmentFund.setInvestmentFund(investmentFund);
+    }
+  },
+};
+</script>
